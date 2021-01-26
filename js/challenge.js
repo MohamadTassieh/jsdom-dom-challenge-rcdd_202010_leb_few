d5b9a329e7142a4772a005b1d52cc92896163221
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2021-01-26 18:14:01 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -16,3 +16,36 @@ document.getElementById("minus").addEventListener("click", function() {
   counterLoad.innerText = counter;
  }
 );
+
+let pauseBtn = documentgetElementById("pause")
+  pauseBtn.addEventListener("click",function () {
+    if(pauseBtn.innerText === "pause"){
+    document.getElementById("minus").disabled = true;
+    document.getElementById("plus").disabled = true;
+    document.getElementById("heart").disabled = true;
+    pauseBtn.innerText="resume";
+    clearInterval(intervalID);}
+  else{
+    intervalID = window.setInterval(count, 1000);
+    document.getElementById("minus").disabled = false;
+    document.getElementById("plus").disabled = false;
+    document.getElementById("heart").disabled = false;
+    pauseBtn.innerText="pause";
+  }
+});
+  document.getElementById("submit").addEventListener("click",function(event){
+    event.preventDefault()
+    let inputText=document.getElementById("comment-input");
+    if(inputText.value===""){
+    alert("enter a comment") }
+    else{ pCmt= document.createElement("p");
+    pCmt.innerText = inputText.value;
+    document.querySelector(".comments").appendChild(pCmt);}
+  });
+  
+   document.getElementById("heart").addEventListener("click",function () {
+  let list =document.getElementByClass(".likes");
+   let listItem = document.createElement("li");
+   listItem.innerText=`❤️ this count ${counter}`
+  list.appendChild(listItem);
+});
